c990eb82202cac54c3a9b2fa1381e0302d30a0ae
davilos <daviloscostagg@hotmail.com>
2022-07-27 09:27:11 -0300

    Sincronização entre processos

---
+++ 04multiprocessamento/sinc_processos_v1.py
@@ -0,0 +1,39 @@
+import multiprocessing
+
+
+def depositar(saldo: int) -> None:
+    for _ in range(10_000):
+        saldo.value += 1
+
+
+def sacar(saldo: int) -> None:
+    for _ in range(10_000):
+        saldo.value -= 1
+
+
+def realizar_transacoes(saldo: int) -> None:
+    pc1 = multiprocessing.Process(
+        target=depositar,
+        args=(saldo,)
+    )
+    pc2 = multiprocessing.Process(
+        target=sacar,
+        args=(saldo,)
+    )
+
+    pc1.start()
+    pc2.start()
+
+    pc1.join()
+    pc2.join()
+
+
+if __name__ == '__main__':
+    saldo = multiprocessing.Value('i', 100)
+
+    print(f'Saldo inicial: {saldo.value}')
+
+    for _ in range(10):
+        realizar_transacoes(saldo)
+
+    print(f'Saldo final: {saldo.value}')
