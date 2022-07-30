70a0b2482807cc6341517c367d714861741e3c65
davilos <daviloscostagg@hotmail.com>
2022-07-30 13:30:26 -0300

    using the lib aiofiles and reading the file "texto.txt"

---
+++ 05async/as_aiofiles.py
@@ -0,0 +1,22 @@
+import asyncio
+import aiofiles
+
+
+async def exemplo_arq1() -> None:
+    async with aiofiles.open('05async/texto.txt') as arquivo:
+        conteudo = await arquivo.read()
+    print(conteudo)
+
+
+async def exemplo_arq2() -> None:
+    async with aiofiles.open('05async/texto.txt') as arquivo:
+        async for linha in arquivo:
+            print(linha)
+
+
+def main() -> None:
+    asyncio.run(exemplo_arq2())
+
+
+if __name__ == '__main__':
+    main()
