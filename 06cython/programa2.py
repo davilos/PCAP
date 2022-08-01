beec7e6a33a68933a9f671f09a133b3bd03968cc
davilos <daviloscostagg@hotmail.com>
2022-08-01 12:50:11 -0300

    compilation with language C

---
+++ 06cython/programa2.py
@@ -0,0 +1,21 @@
+import datetime
+import computa
+
+
+def main() -> None:
+    inicio: datetime.datetime = datetime.datetime.now()
+
+    computa.computar(fim=50_000_000)
+
+    tempo: datetime.timedelta = datetime.datetime.now() - inicio
+
+    print(f'Terminou em {tempo.total_seconds():.2f} segundos.')
+
+
+if __name__ == '__main__':
+    main()
+
+# Terminou em 13.89 segundos.
+# Terminou em 9.65 segundos. - Cython
+# Terminou em 0.46 segundos. - Tipagem de dados do Cython
+# Terminou em 0.27 segundos. - Nogil (apenas linguagem C) 
