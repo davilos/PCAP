21a2234c30c3a20a101e936188566a2ef078d244
davilos <daviloscostagg@hotmail.com>
2022-08-01 13:19:16 -0300

    compilation with Cython and Multiprocesses

--- 04multiprocessamento/python_multiprocessos.py
+++ 04multiprocessamento/python_multiprocessos.py
@@ -1,6 +1,5 @@
 import datetime
-import math
-
+import computa
 import multiprocessing
 from concurrent.futures.process import ProcessPoolExecutor as Executor
 
@@ -16,23 +15,17 @@ def main() -> None:
             ini = 50_000_000 * (n - 1) / qtd_cores
             fim = 50_000_000 * n / qtd_cores
             print(f'Core {n} processando de {ini} até {fim}')
-            executor.submit(computar, inicio=ini, fim=fim)
+            executor.submit(computa.computar, inicio=ini, fim=fim)
 
     tempo: datetime.timedelta = datetime.datetime.now() - inicio
 
     print(f'Terminou em {tempo.total_seconds():.2f} segundos.')
 
 
-def computar(fim: int, inicio: int = 1) -> None:
-    pos: int = inicio
-    fator: int = 1000 * 1000
-
-    while pos < fim:
-        pos += 1
-        math.sqrt((pos - fator) * (pos - fator))
-
-
 if __name__ == '__main__':
     main()
 
-# Terminou em 6.04 segundos.
+# Terminou em 6.04 segundos. - Apenas multiprocessos
+# Terminou em 4.16 segundos. - Cython
+# Terminou em 0.22 segundos. - Tipagem de dados do Cython
+# Terminou em 0.15 segundos. - Nogil (apenas linguagem C)
